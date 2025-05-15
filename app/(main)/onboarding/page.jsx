392e2b0fd1ad85b5fd8097b717777b7bb8391e17
Mahamure Kedar <kedarmahamura12@gmail.com>
2025-05-15 17:47:05 +0530

    first commit

---
+++ app/(main)/onboarding/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import OnboardingForm from './_components/onboarding-form'
+import { industries } from '@/data/industries'
+import { getUserOnboardingStatus } from '@/actions/user';
+import { redirect } from 'next/navigation';
+import { toast } from 'sonner';
+
+const OnboardingPage = async () => {
+
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <main>
+      <OnboardingForm industries={industries} />
+    </main>
+  )
+}
+
+export default OnboardingPage
